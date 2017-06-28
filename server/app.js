383fa7ca2e2a82d157e07cd0f6d8c3d9e835262e
jeffkrantz <jjkrantz@gmail.com>
2017-06-28 09:49:09 -0500

    sessions

--- server/app.js
+++ server/app.js
@@ -2,12 +2,25 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     path = require('path'),
+    session = require('express-session'),
     Book = require('./models/Book');
     require('./db/db.js');
 
+app.use(session( {
+  secret: "I'm a secret password",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {secure: false}
+}));
+
 var BookController = require('./controllers/BookController');
+var UserController = require('./controllers/UserController');
 
 app.use('/books', BookController);
+app.use('/users', UserController);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 server.listen(3000, function(){
   console.log('listening on port 3000');
--- server/controllers/BookController.js
+++ server/controllers/BookController.js
@@ -5,6 +5,18 @@ var bodyParser = require('body-parser');
 
 router.use(bodyParser.urlencoded({extended: true}));
 
+// GET request to /books/view
+router.get('/view', function(request, response){
+  Book.find(function(error, books){
+    if(request.session.loggedIn === true){
+      var myBooks = {allBooks: books, loggedIn: true};
+      response.render('books', myBooks);
+    }else{
+      response.redirect('/users/login');
+    }
+  })
+})
+
 router.get('/', function(request, response){
   //send all the books
   Book.find(function(error, books){
